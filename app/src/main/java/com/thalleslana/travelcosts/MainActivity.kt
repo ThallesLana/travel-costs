b7609d56b0e5f5faa70be8ab8ce3070c2cba374b
ThallesLana <thalles.lana1@gmail.com>
2024-01-22 19:17:15 -0300

    finish project

--- app/src/main/java/com/thalleslana/travelcosts/MainActivity.kt
+++ app/src/main/java/com/thalleslana/travelcosts/MainActivity.kt
@@ -32,18 +32,52 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun validateDistance(): Boolean {
+        if(binding.editDistance.text.toString().isBlank()){
+            binding.editDistance.error = getString(R.string.validate_distance)
+            Toast.makeText(applicationContext, R.string.validate_distance, Toast.LENGTH_SHORT).show()
+            return false
+        } else {
+            return true
+        }
+    }
+    private fun validatePrice(): Boolean {
+        if(binding.editPrice.text.toString().isBlank()){
+            binding.editPrice.error = getString(R.string.validate_price)
+            Toast.makeText(applicationContext, R.string.validate_price, Toast.LENGTH_SHORT).show()
+            return false
+        } else {
+            return true
+        }
+    }
+    private fun validateAutonomy(): Boolean {
+        if(binding.editAutonomy.text.toString().isBlank() || binding.editAutonomy.text.toString().toFloat() == 0f){
+            binding.editAutonomy.error = getString(R.string.validate_autonomy)
+            Toast.makeText(applicationContext, R.string.validate_autonomy, Toast.LENGTH_SHORT).show()
+            return false
+        } else {
+            return true
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return validateDistance() && validatePrice() && validateAutonomy()
+    }
     private fun calculate() {
-        val distance = binding.editDistance.text.toString().toFloat();
-        val price = binding.editPrice.text.toString().toFloat();
-        val autonomy = binding.editAutonomy.text.toString().toFloat();
+        if(isValid()){
+            val distance = binding.editDistance.text.toString().toFloat();
+            val price = binding.editPrice.text.toString().toFloat();
+            val autonomy = binding.editAutonomy.text.toString().toFloat();
 
-        val total = (distance * price) / autonomy;
+            val total = (distance * price) / autonomy;
 
-        // formatação de numero para decimal com 2 casas.
-        binding.viewCost.text = "R$ ${"%.2f".format(total)}";
+            // formatação de numero para decimal com 2 casas.
+            binding.viewCost.text = "R$ ${"%.2f".format(total)}";
 
 
-        // Toast Notify
-        Toast.makeText(applicationContext, "Calculo Feito com Sucesso!", Toast.LENGTH_SHORT).show();
+            // Toast Notify
+            Toast.makeText(applicationContext, "Calculo Feito com Sucesso!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
